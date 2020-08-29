7f2fa0537f1e1adb8e4410859280e7b207a5f71f
arash <arashghelman77@gmail.com>
2020-08-29 10:15:51 +0430

    .

---
+++ Sample02/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample02.Controllers
+{
+    [RoutePrefix("Product")]
+    public class ProductController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
